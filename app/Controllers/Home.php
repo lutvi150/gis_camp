1eb70130b61cf8401193972bf4b0eae0fa0333ce
Rahmat Lutvi Furkon <lutvi1500@gmail.com>
2024-10-04 08:26:39 +0700

    menu first

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,34 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+    function home()
+    {
+        $data['owl'] = [
+            'upload/owl/5.jpg',
+            'upload/owl/4.jpg'
+        ];
+        $data['testimoni'] = (object)[
+            (object)[
+                'nama' => 'Budi',
+                'alamat' => 'Bireuen',
+                'foto' => 'upload/profil.jpg',
+                'testimoni' => 'Saya sangat puas dengan pelayanan yang diberikan . Fasilitasnya sangat lengkap dan harga yang terjangkau.'
+            ],
+            (object)
+            [
+                'nama' => 'Ani',
+                'alamat' => 'Banda Aceh',
+                'foto' => 'upload/profil.jpg',
+                'testimoni' => 'Wisata camping di danau lut tawar sangat menyenangkan. Kita bisa menikmati pemandangan alam yang indah.'
+            ],
+            (object)
+            [
+                'nama' => 'Lili',
+                'alamat' => 'Banda Aceh',
+                'foto' => 'upload/profil.jpg',
+                'testimoni' => 'Udaranya sangat indah. Tidak terlalu ramai, harga yang terjangkau, fasilitas yang lengkap.'
+            ]
+        ];
+        return view('home', $data);
+    }
 }
